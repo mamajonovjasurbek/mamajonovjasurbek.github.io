05fa9d19045646a7651f648ae71e28093f9aee07
mamajonovjasurbek <85944913+mamajonovjasurbek@users.noreply.github.com>
2021-06-26 11:09:12 +0500

    Add files via upload
    My files))

---
+++ js/script.js
@@ -0,0 +1,9 @@
+// $(function(){
+//     $('.subscribe__block').slick({
+//         nextArrow:'<button type="button" class="slick-next"></button>',
+//         prevArrow:'<button type="button" class="slick-prev"></button>',
+//         infinite: false,
+//         dots: true
+//     });
+
+// });
